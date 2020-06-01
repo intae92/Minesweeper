f8b4b8dcff533b37dad240944cc110fcfc2783b5
intae <kangit2013@gmail.com>
2020-06-01 23:30:37 +0900

    ﻿ 타이머 구현, 깃발 개수 카운트 구현 마우스 오른쪽 클릭시 나오는 깃발, 물음표 이모티콘 보이게 구현 지뢰 밟았을 때 폭탄 이모티콘 보이게 구현 ​ 그외 해야 할일 1.승리 했을때 2.게임판 위에 입력창 다시 구현 ​

--- Minesweeper/mine.js
+++ Minesweeper/mine.js
@@ -2,12 +2,13 @@
 //백그라운드 포지션을 sprite로 필요한 부분만 보이게끔
 const execBtn = document.querySelector("#exec");
 const tbody = document.querySelector("#table > tbody");
+
 let stopFlag = false; //지뢰 밟으면 펑일때
 let stopClassListRemove = true;
 let tdDivClicked = false; //마우스로 누르고 td범위 나가면 테두리 원상복귀 시키기 위한 flag
 // let rightClicked = true; //오른쪽 마우스 클릭시 테두리 변화 안주기 위해서
 let cliceStart = false; //마우스 클릭후 move 이벤트 실행 시키기위해
-
+const flags = 40;//깃발 개수
 //지뢰찾기 theat tr 반응형으로 가로 크기 변하게 하기 위해서
 const table = document.querySelector("table")
 const theadTr = table.children[0].children[0];
@@ -29,31 +30,64 @@ function resetData(){ //데이터셋, 지뢰화면 리셋
 }
 
 function rightClickedContext(text){ //오른쪽 클릭 된 ! ? 변화 안되게
-    if(text === "!" || text === "?"){
+    // console.log(text)
+    if(text.contains("flagImage") || text.contains("questionImage")){
         return true;
-    }else {
+    } else {
         return false;
     }
+    // if(text === "!" || text === "?"){
+    //     return true;
+    // }else {
+    //     return false;
+    // }
 }
+function flagCount(HOR, VER){//물음표 갯수 세기
+    let count=0;
+    for(let i=0; i<HOR; i++){
+        for(let j=0; j<VER; j++){
+            if(tbody.children[i].children[j].children[0].classList.contains("flagImage")){
+                count++;
+            }
+        }
+    }
+    
+    // return flags - count < 0 ? console.log('error') : flags-count;
+    return flags - count;
+}
+
 let rightClicked = true;//오른쪽 버튼 누리고 마우스 이벤트 구현 안되게 하기위해서
 function handleRightClick(e){//마우스 오른쪽 클릭 물음표
     // console.log("handleRightClick");
-    console.log(e.target);
+    console.log(e.target)
     console.dir(e.target);
+    console.log(e.target.classList.contains("flagImage"))
     e.preventDefault();
     if(stopFlag){
         return;
     }
     e.target.classList.add("tdDivDefaultBorder");
+    
     rightClicked = false;//오른쪽 버튼 누리고 마우스 이벤트 구현 안되게 하기위해서
-    if(e.target.textContent === ''){
-        e.target.textContent = "!"
-    } else if(e.target.textContent === "!"){
-        e.target.textContent = "?"
-    } else if(e.target.textContent === "?"){
-        e.target.textContent = "";
+    if(!e.target.classList.contains("flagImage") && !e.target.classList.contains("questionImage")){
+        e.target.classList.add("flagImage");
+    } else if(e.target.classList.contains("flagImage")){
+        // e.target.textContent = "?"
+        console.log('ddd')
+        e.target.classList.remove("flagImage");
+        e.target.classList.add("questionImage")
+    } else if(e.target.classList.contains("questionImage")){
+        e.target.classList.remove("questionImage");
     }    
 
+    //깃발 개수 세기 위해서
+    const flagText = document.querySelector("#flagText");
+    let count = flagCount(HOR, VER);
+    flagText.textContent = `${count < 10 ? `00${count}` : count < 100 ? `0${count}`: count}`;
+    if(count < 0){
+        e.target.textContent = '';
+        flagText.textContent = '000';
+    }
 }
 
 function clickZreo(zero, row, col){//지뢰 수 0 클릭시 주변 오픈
@@ -61,30 +95,51 @@ function clickZreo(zero, row, col){//지뢰 수 0 클릭시 주변 오픈
         for(let i=-1; i<2; i++){
             for(let j=-1; j<2; j++){
                 if(row+i >=0 && col+j>=0 && row+i < VER && col+j < HOR){
-                    if(tbody.childNodes[row+i].childNodes[col+j].textContent === ''){
-                        tbody.childNodes[row+i].childNodes[col+j].textContent = dataset[row+i][col+j];
+                    if(!tbody.children[row+i].children[col+j].children[0].classList.contains("flagImage") 
+                    && !tbody.children[row+i].children[col+j].children[0].classList.contains("questionImage") 
+                    && !tbody.children[row+i].children[col+j].children[0].classList.contains("clicked")){
+                        // console.log(row+i, col+j)
+                        tbody.children[row+i].children[col+j].children[0].textContent = dataset[row+i][col+j] === 0 ? '' : dataset[row+i][col+j];
+                        // tbody.children[row+i].children[col+j].children[0].textContent = dataset[row+i][col+j];
+                        tbody.children[row+i].children[col+j].children[0].classList.add("clicked");
+                        tbody.children[row+i].children[col+j].children[0].classList.remove("tdDivDefaultBorder");
                         if(dataset[row+i][col+j] === 0){
                             
                             clickZreo(dataset[row+i][col+j], row+i, col+j);
                         }
                     }
+                    // if(tbody.childNodes[row+i].childNodes[col+j].textContent === ''){
+                    //     tbody.childNodes[row+i].childNodes[col+j].textContent = dataset[row+i][col+j];
+                    //     if(dataset[row+i][col+j] === 0){
+                            
+                    //         clickZreo(dataset[row+i][col+j], row+i, col+j);
+                    //     }
+                    // }
                     
                 }
             }
         }
     }
 }
+
 function idRowCol(strId){//문자열형태의 id를 원하는 형태, 숫자인 row와 col로 바꿈
     return strId.split(',').map(v => Number(v));
 }
+function victory(){//승리 조건
+
+}
+
 function handleLeftClick(e){//mouseup
-    console.log('mouseup handleLeftClick')
-    console.log(e.target);
-    if(rightClickedContext(e.target.textContent)){//오른쪽 클릭 된 ! ? 변화 안되게
+    // console.log('mouseup handleLeftClick')
+    // console.log(e.target.classList)
+    // console.dir(e.target)
+    
+    victory();//승리조건
+    if(rightClickedContext(e.target.classList)){//오른쪽 클릭 된 ! ? 변화 안되게
         return;
     }
     if(rightClicked){//오른쪽 버튼 누리고 마우스 이벤트 구현 안되게 하기위해서
-        console.log('mouseupRighted')
+        // console.log('mouseupRighted')
         mouseDowning = false;
         
         if(stopFlag){
@@ -112,10 +167,13 @@ function handleLeftClick(e){//mouseup
         if(e.target.textContent === ''){
             if(dataset[row][col] === "X"){
             
-                e.target.textContent = '펑';
+                
+                e.target.classList.add("bombImage");
+                stopCounter();//시간 멈춤
                 stopFlag = true; //클릭 안되게 함
             } else {
-                e.target.textContent = dataset[row][col];
+                e.target.textContent = dataset[row][col] === 0 ? '' : dataset[row][col];
+                // e.target.textContent = dataset[row][col];
                 clickZreo(dataset[row][col], row, col);//지뢰 수 0 클릭시 주변 오픈
             }
         }
@@ -126,7 +184,7 @@ function handleLeftClick(e){//mouseup
 let mouseDowning = false; //마우스 누르고 있는중
 function tdDivMouseDown(e){//마우스 누를때 테두리 변화
     // console.log('donw')
-    if(rightClickedContext(e.target.textContent)){//오른쪽 클릭 된 ! ? 변화 안되게
+    if(rightClickedContext(e.target.classList)){//오른쪽 클릭 된 ! ? 변화 안되게
         return;
     }
     rightClicked = true;//오른쪽 버튼 누리고 마우스 이벤트 구현 안되게 하기위해서
@@ -141,7 +199,7 @@ function tdDivMouseDown(e){//마우스 누를때 테두리 변화
 
 function tdDivMouseOut(e){
     // console.log('tdDivMouseOut')
-    if(rightClickedContext(e.target.textContent)){//오른쪽 클릭 된 ! ? 변화 안되게
+    if(rightClickedContext(e.target.classList)){//오른쪽 클릭 된 ! ? 변화 안되게
         return;
     }
     
@@ -160,7 +218,7 @@ function tdDivMouseOut(e){
 }
 function tdDivMouseMove(e){
     // console.log('tdDivMouseMove')
-    if(rightClickedContext(e.target.textContent)){//오른쪽 클릭 된 ! ? 변화 안되게
+    if(rightClickedContext(e.target.classList)){//오른쪽 클릭 된 ! ? 변화 안되게
         return;
     }
     if(rightClicked){//마우스 오른쪽 누르면 마우스이벤트 구현 안되게 하기 위해서
@@ -279,9 +337,15 @@ function emotionClickMouseUp(e){
     e.target.classList.add("emotionDefaultBorder");
     
 }
-
+let counter;
+let timerCounter;
+function stopCounter(){
+    clearInterval(timerCounter);
+}
 function emotionClick(e){
+    stopCounter();
     execClick();//리셋하기
+    
 }
 function theadAddDiv(){//thead에 깃발수 시간 이모티콘 넣기 위해
     // console.dir(theadTr)
@@ -290,8 +354,15 @@ function theadAddDiv(){//thead에 깃발수 시간 이모티콘 넣기 위해
     // console.dir(theadTh);
     const theadThContainer = document.createElement("div");
     const flagDiv = document.createElement("div");
+    const flagDivSpan = document.createElement("span");
     const emotionDiv = document.createElement("div");
     const timerDiv = document.createElement("div");
+    const timerDivSpan = document.createElement("span");
+    timerDiv.append(timerDivSpan);
+    timerDivSpan.innerText = "000";
+    flagDivSpan.id = 'flagText';
+    flagDiv.append(flagDivSpan);
+    flagDivSpan.innerHTML = `${flags < 10 ? `00${flags}` : flags < 100 ? `0${flags}`: flags}`;
     theadThContainer.append(flagDiv, emotionDiv, timerDiv);
     theadTh.append(theadThContainer);
 
@@ -300,9 +371,24 @@ function theadAddDiv(){//thead에 깃발수 시간 이모티콘 넣기 위해
     emotionDiv.addEventListener("mousedown", emotionClickMouseDown);//이모티콘 마우스로 누를때
     emotionDiv.addEventListener("mouseout", emotionClickMouseUp);
     // console.log(emotionDiv);
+
+    counter = function(){
+        let num = 0;
+        timerCounter = setInterval(function(){
+            num++;
+            timerDivSpan.innerText = `${num < 10 ? `00${num}` : num > 99 ? num : `0${num}`}`;
+            
+        },1000);
+    }
+
+    counter();
+    
+    
 }
 
-const board = document.querySelector("#game-board");
+
+
+// const board = document.querySelector("#game-board");//게임판 크기 고정 시키기 위해서
 function execClick(e){//실행 버튼 클릭시
     // e.preventDefault();
     HOR = parseInt(document.querySelector("#hor").value);//가로
@@ -320,15 +406,16 @@ function execClick(e){//실행 버튼 클릭시
     createMineTable(HOR, VER);//지뢰찾기 테이블 만들기
     planingMines(HOR, VER, MINE);//지뢰 심기
 
-    let boardWidth = board.clientWidth + board.clientTop + board.clientLeft+11;//게임판 Width 크기 고정 시키기 위해서
-    let boardHeight = board.clientHeight + board.clientTop + board.clientLeft;
-    console.dir(board)
-    console.log(board.clientHeight)
-    console.log(board.clientLeft)
-    console.log(board.clientTop)
-    console.log(board.clientWidth)
-    console.log(boardWidth);
-    console.log(boardHeight);
+    // 버튼 눌러도 게임판 크기 고정되게 해야함.
+    // let boardWidth = board.clientWidth + board.clientTop + board.clientLeft;//게임판 Width 크기 고정 시키기 위해서
+    // let boardHeight = board.clientHeight + board.clientTop + board.clientLeft;
+    // console.dir(board)
+    // console.log(board.clientHeight)
+    // console.log(board.clientLeft)
+    // console.log(board.clientTop)
+    // console.log(board.clientWidth)
+    // console.log(boardWidth);
+    // console.log(boardHeight);
     
 
 }
